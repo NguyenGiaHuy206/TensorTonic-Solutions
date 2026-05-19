4424620007f2374f15a2bcc20921a404f194fa7e
HuyNguyen206 <nguyenhuyct2006@gmail.com>
2026-05-19 09:31:20 +0700

    Update solution for mean-squared-error

--- mean-squared-error/mean-squared-error.py
+++ mean-squared-error/mean-squared-error.py
@@ -5,8 +5,7 @@ def mean_squared_error(y_pred, y_true):
     Returns: float MSE
     """
     # Write code here
-    if len(y_pred) != len(y_true):
-        return None
+
     y_pred = np.array(y_pred)
     y_true = np.array(y_true)
     loss = np.sum((y_pred - y_true) ** 2)
